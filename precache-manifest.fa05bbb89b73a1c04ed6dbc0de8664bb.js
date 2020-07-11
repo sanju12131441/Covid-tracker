5150f958dc32c0d753d48f8c1d48e17f7eff9832
sanju12131441 <saisanjanreddy@gmail.com>
2020-07-12 02:51:58 +0530

    Updates

---
+++ precache-manifest.fa05bbb89b73a1c04ed6dbc0de8664bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8e87e4282109e6f6212ff8e53ffed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "488e3062eb810f7c8d7c",
+    "url": "/static/css/main.9711983c.chunk.css"
+  },
+  {
+    "revision": "a0d33e1ea1a47e730df4",
+    "url": "/static/js/2.1a3dfd2b.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.1a3dfd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488e3062eb810f7c8d7c",
+    "url": "/static/js/main.99de5595.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-tracker/precache-manifest.82fa91bc7fb78718692209d6da1be31f.js"
+  "/precache-manifest.fa05bbb89b73a1c04ed6dbc0de8664bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Covid-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
